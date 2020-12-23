a30eea4016022179dd37659f63bb07ce497024a2
Simon <simon.perriard@epfl.ch>
2020-12-23 17:03:42 +0100

    Added gitignore

--- vigrouter/lib/containers/.double-chain-impl.o.d
+++
@@ -1,5 +0,0 @@
-double-chain-impl.o: \
- /home/simon/Documents/EPFL/bachelor_project/vnds/nf/vigrouter/../lib/containers/double-chain-impl.c \
- /usr/include/stdc-predef.h \
- /home/simon/Documents/EPFL/bachelor_project/dpdk/x86_64-native-linuxapp-gcc/include/rte_config.h \
- /home/simon/Documents/EPFL/bachelor_project/vnds/nf/vigrouter/../lib/containers/double-chain-impl.h
